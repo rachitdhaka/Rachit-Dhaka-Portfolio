fc9e44d1657af46c7b95423fd199b54b870dc0e4
Rachit Dhaka <rachitdhaka29@gmail.com>
2025-07-17 00:01:08 +0530

    changes

---
+++ src/TestApp.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function TestApp() {
+  return (
+    <div className="min-h-screen bg-gray-100 p-8">
+      <h1 className="text-4xl font-bold text-gray-900">Hello World!</h1>
+      <p className="text-lg text-gray-600 mt-4">
+        This is a test to see if React is working.
+      </p>
+    </div>
+  );
+}
+
+export default TestApp;
