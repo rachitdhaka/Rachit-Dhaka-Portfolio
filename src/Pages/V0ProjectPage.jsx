fc4dc33fc1e3ace922e09539e5988091b25490ba
Rachit Dhaka <rachitdhaka29@gmail.com>
2025-07-18 18:32:32 +0530

    Changes

--- src/Pages/V0ProjectPage.jsx
+++ src/Pages/V0ProjectPage.jsx
@@ -7,6 +7,7 @@ import game from "../assets/Images/game.png";
 import V0projectCard from "../components/V0projectCard";
 import HeikiBackground from "../components/HeikiBackground";
 import weather from "../assets/Images/weather.png";
+import chatbot from "../assets/chatbot application.png";
 const projects = [
   {
     id: 1,
@@ -63,7 +64,7 @@ const projects = [
     name: "Chat Bot Application",
     detail:
       "Chat Bot Application - Using Google AI to create a chat bot application",
-    logo: weather,
+    logo: chatbot,
     url: "https://chatbot-rachit.vercel.app/",
     techStack: ["React", "Tailwind" , "Api" , "Google AI"],
     githubUrl: "http://github.com/rachitdhaka/Chat-Bot-Application",
