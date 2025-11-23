7b344fc177dcc3118cb38c2c391f0154d84c52c8
Rachit Dhaka <rachitdhaka29@gmail.com>
2025-11-24 00:14:22 +0530

    cahnges

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,11 +3,13 @@ import { GeistSans } from "geist/font/sans";
 import { GeistMono } from "geist/font/mono";
 import "./globals.css";
 import { ThemeProvider } from "@/Providers/Theme-providers";
-
 export const metadata: Metadata = {
   title: "Rachit Dhaka",
   description:
     "Rachit Dhaka's personal website showcasing projects, blog posts, and contact information.",
+  icons: {
+    icon: "/Images/dp.jpg",
+  },
 };
 
 export default function RootLayout({
