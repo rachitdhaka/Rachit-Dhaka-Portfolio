80c86b5f6787d76b4b80a3d576a3e45694850ff8
Rachit Dhaka <rachitdhaka29@gmail.com>
2025-12-06 20:17:07 +0530

    Meta data

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,11 +4,43 @@ import { GeistMono } from "geist/font/mono";
 import "./globals.css";
 import { ThemeProvider } from "@/Providers/Theme-providers";
 import { ThemeShortcutListener } from "@/components/ThemeShortcutListener";
+import ogImg from "@/public/Images/OgImage.jpg"
+
+
 export const metadata: Metadata = {
   title: "Rachit Dhaka",
   description:
     "Rachit Dhaka's personal website showcasing projects, blog posts, and contact information.",
+  
+   openGraph: {
+    title: "Rachit Dhaka Portfolio",
+    description: "Rachit Dhaka's personal website showcasing projects, blog posts, and contact information.",
+    url: "https://rachitdhaka.me",
+    siteName: "Rachit Dhaka Portfolio",
+    images: [
+      {
+        url: ogImg.src,
+        width: 1200,
+        height: 630,
+        alt: "My Website Social Card",
+      },
+    ],
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Rachit Dhaka Portfolio",
+    description: "Rachit Dhaka's personal website showcasing projects, blog posts, and contact information.",
+    images: [ogImg.src],
+  },
 };
+
+
+
+
+
+
+
 
 export default function RootLayout({
   children,
