2eee6b78616bb871cc5216047cac62c89ac8da5f
Rachit Dhaka <rachitdhaka29@gmail.com>
2026-01-10 14:39:59 +0530

    changes

--- components/ResumeSection.tsx
+++ components/ResumeSection.tsx
@@ -4,6 +4,7 @@ import { Button } from "./ui/button";
 import Link from "next/link";
 import Fileicon from "@/public/IconSvg/Fileicon";
 import FileDescriptionIcon from "./ui/file-description-icon";
+import SendIcon from "./ui/send-icon";
 
 export const ResumeSection = memo(() => {
   const handleGetInTouch = useCallback(() => {
@@ -17,18 +18,19 @@ export const ResumeSection = memo(() => {
     <div className="py-4 px-2 flex flex-col sm:flex-row gap-2 w-full sm:w-auto">
       <Button className="cursor-pointer w-full sm:w-auto " asChild>
         <Link href="/Documents/Resume.pdf" download="Rachit_Dhaka_Resume.pdf">
-          <FileDescriptionIcon/>
+          <FileDescriptionIcon />
           Resume
         </Link>
       </Button>
-      <Button
-        className="cursor-pointer w-full sm:w-auto"
-        variant="secondary"
+
+
+      <button
+        className="cursor-pointer bg-secondary text-secondary-foreground hover:bg-secondary/80 h-9 px-4 py-2 has-[>svg]:px-3 rounded-md flex items-center gap-2 "
         onClick={handleGetInTouch}
       >
-        
+        <SendIcon />
         Get in Touch
-      </Button>
+      </button>
     </div>
   );
 });
